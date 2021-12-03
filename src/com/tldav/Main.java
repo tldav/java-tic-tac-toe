16eb575c7752e7d5f3fb655b8aa7df8c840d30e5
tldav <tldavis09@yahoo.com>
2021-12-03 12:54:13 -0600

    add grid input validation

--- src/com/tldav/Main.java
+++ src/com/tldav/Main.java
@@ -1,10 +1,22 @@
 package com.tldav;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        String sampleGrid = "X O X\nO O O\nX X O";
-        System.out.println(sampleGrid);
+//        String sampleGrid = "X O X\nO O O\nX X O";
+//        System.out.println(sampleGrid);
+
+
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        if (input.length() != 9) System.out.println("please enter again");
+        if (!input.matches("^[XO_]*$")) System.out.println("wrong");
+
+
     }
 }
