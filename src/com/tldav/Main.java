8a7476b57494924fa7b54b349d4d82c8ee573749
tldav <tldavis09@yahoo.com>
2021-12-03 14:17:34 -0600

    while loop re-runs if invalid characters or too few/many characters are provided

--- src/com/tldav/Main.java
+++ src/com/tldav/Main.java
@@ -1,6 +1,6 @@
 package com.tldav;
 
-import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -12,19 +12,41 @@ public class Main {
 //        System.out.println(sampleGrid);
 
 
-        System.out.print("Enter cells: ");
-        String input = scanner.nextLine();
+//        if (input.length() != 9) System.out.println("Please enter 9 characters.");
+//        if (!input.matches("^[XO_]*$")) System.out.println("Valid characters: X, O, _ do not enter spaces");
+//
+//        System.out.println("---------");
+//        String lineOne = input.substring(0, 3);
+//        String lineTwo = input.substring(3, 6);
+//        String lineThree = input.substring(6, 9);
+//        System.out.println("|" + lineOne.replace("", " ") + "|");
+//        System.out.println("|" + lineTwo.replace("", " ") + "|");
+//        System.out.println("|" + lineThree.replace("", " ") + "|");
+//        System.out.println("---------");
 
-        if (input.length() != 9) System.out.println("please enter again");
-        if (!input.matches("^[XO_]*$")) System.out.println("wrong");
 
-        String lineOne = input.substring(0, 3);
-        String lineTwo = input.substring(3, 6);
-        String lineThree = input.substring(6, 9);
-        System.out.println("|" + lineOne.replace("", " ") + "|");
-        System.out.println("|" + lineTwo.replace("", " ") + "|");
-        System.out.println("|" + lineThree.replace("", " ") + "|");
+        while (true) {
+            System.out.print("Enter cells: ");
+            String input = scanner.nextLine().toUpperCase();
 
+            if (input.length() != 9) {
+                System.out.println("Please enter 9 characters consisting of _, X, O.");
+                continue;
+            }
+            if (!input.matches("^[XO_]*$")) {
+                System.out.println("Valid characters: _, X, O. Do not enter spaces");
+                continue;
+            }
+            System.out.println("---------");
+            String lineOne = input.substring(0, 3);
+            String lineTwo = input.substring(3, 6);
+            String lineThree = input.substring(6, 9);
+            System.out.println("|" + lineOne.replace("", " ") + "|");
+            System.out.println("|" + lineTwo.replace("", " ") + "|");
+            System.out.println("|" + lineThree.replace("", " ") + "|");
+            System.out.println("---------");
+            break;
+        }
 
 
     }
